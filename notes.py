7b5214cc98a12e45e86e9e0851b130c2cb6e358a
PTHARRISH <harrishpt@gmail.com>
2024-01-20 18:39:53 +0530

    display the name,age and job

---
+++ notes.py
@@ -0,0 +1,53 @@
+# Create React Application
+
+# What is npm and npx
+# npm:
+# npm is node package manager for javascript.
+# It helps you manage all the 3rd party packages and libraries that you will be installing for your application.
+# It is installed automatically when you install Node JS
+
+# npx
+# npx is a node package runner
+# It is used to download and run a package temporarily
+
+# Install node js from official site
+# check node version:  node -v 
+# current version- v20.9.0
+# check npm version: npm -v
+# current version- 10.2.5
+
+# create react app - npx create-react-app reactapp_name
+
+
+# Introduction to jsx
+# What is JSX
+# JSX is a syntax extension to javascript 
+# It allows us to define React element using syntax that looks very similiar to HTML
+# It is used to define the look of the UI
+# In other words it is used to define the structure of React Components
+
+# HTML vs JSX
+# Unlike HTML,JSX couples the rendering logic with other UI logic such as event handling,
+# state changing, data displaying etc..
+
+# const imageDescription="This is a beautiful mountain"
+# <div>
+#   <h1>Main heading</h1>
+#   <img src="https://img.ur/9ydjbdx" alt="mountain"/>
+#   <p>{imageDescription}</p>
+# </div>
+
+
+# display the name,age and job 
+# Open App.js type inside a function
+# function App() {
+#   return (
+#     <div className="App">
+#      <h3> Full Name: Harrish </h3>
+#      <p>Age: 28</p>
+#      <p>Job: VIP</p>
+#    </div>
+#   );
+# } 
+
+ 
