565f16321fb5f6275f5bc8181c54c63f7ceb25c5
PTHARRISH <harrishpt@gmail.com>
2024-01-20 18:37:55 +0530

    display the name,age and job

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <h3> Full Name: Harrish </h3>
+     <p>Age: 28</p>
+     <p>Job: VIP</p>
+   </div>
   );
 }
 
